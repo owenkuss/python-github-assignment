0f3181a6e2f7df97e8dad9cd899fb72e14066b30
Owen Kuss <owenkuss@usf.edu>
2025-11-22 14:33:49 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+#Greet the User
 print("Welcome to my Python program!")
 
+#Ask the User for Input (Weekly Savings)
 money = input("How much money did you save this week?)
 
+#Forcast Monthly Savings based on Weekly Savings
 money = float(money)
 monthly = money * 4
 
+#Print forcasted Monthly Savings
 print(f"You are on track to save ${monthly} this month.")
 
+#Basic Error Handling
 try:
     money = float(money)
 except ValueError:
